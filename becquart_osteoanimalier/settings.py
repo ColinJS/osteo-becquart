a3bdbfcca2fa00fe702d748088a298a87aab1fbe
ColinJS <js_colin@hotmail.fr>
2021-03-13 20:58:47 +0100

    gunicorn

--- becquart_osteoanimalier/settings.py
+++ becquart_osteoanimalier/settings.py
@@ -137,7 +137,7 @@ WHITENOISE_USE_FINDERS = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'mystaticfiles')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'becquart_osteoanimalier/static'),
                     os.path.join(BASE_DIR, 'becquart_osteoanimalier/galery/static'),
